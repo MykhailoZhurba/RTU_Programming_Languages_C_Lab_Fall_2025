bb7aec188258e05fe5ae6b6d205eeeccce89ba82
MykhailoZhurba <mykhailo.zhurba0@gmail.com>
2025-09-15 07:45:55 +0000

    Practice second try

--- src/hello.c
+++ src/hello.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
   // This is my first C program
-  printf("Hello, RTU World from C Lab in 2026!\n");
+  printf("Hello from Mykhailo, RTU World from C Lab in 2026!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
