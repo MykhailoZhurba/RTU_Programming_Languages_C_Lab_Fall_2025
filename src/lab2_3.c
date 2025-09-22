bdc774534eaa4684374ff1ae1b01b9d5116bb9f3
MykhailoZhurba <mykhailo.zhurba0@gmail.com>
2025-09-22 08:15:56 +0000

    lab2_3

--- src/lab2_3.c
+++ src/lab2_3.c
@@ -2,15 +2,15 @@
 #include <math.h>  // for sqrt
 
 int is_prime(int n) {
-    if (n < 2) return 0;  // not prime
+    if (n < 2) return 0; 
 
     int limit = (int)sqrt(n);
     for (int i = 2; i <= limit; i++) {
         if (n % i == 0) {
-            return 0; // divisible = not prime
+            return 0; 
         }
     }
-    return 1; // prime
+    return 1; 
 }
 
 int main(void) {
